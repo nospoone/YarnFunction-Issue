5c5cdbf3f1f845d20e6f978c8cbf8612517d127e
Roméo Jr. Labonté <romeo@q-bit.ca>
2022-02-16 12:36:02 -0500

    added custom dice function

---
+++ Assets/YarnFunctions.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using Yarn.Unity;
+
+public class YarnFunctions {
+	// [YarnFunction("dice")]
+	// public static int Dice(int sides) {
+	// 	return Random.Range(1, sides + 1);
+	// }
+}
